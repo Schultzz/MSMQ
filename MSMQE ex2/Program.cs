c1de598bce5d42dd48c3c6f23d605078f9293508
Schultzz <ms.fuge@gmail.com>
2016-09-17 17:56:31 +0200

    Add project files.

---
+++ MSMQE ex2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Messaging;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace QueueApplication
+{
+    class Demo
+    {
+        private MessageQueue reuquestQueue;
+        private MessageQueue replyQueue;
+        private MessageQueue invalidMessageQueue;
+
+        public Demo()
+        {
+            this.reuquestQueue = SetupChannel(@".\Private$\Request");
+            this.replyQueue = SetupChannel(@".\Private$\Reply");
+            this.invalidMessageQueue = SetupChannel(@".\Private$\InvalidMessage");
+
+            replyQueue.MessageReadPropertyFilter.SetAll();
+            ((XmlMessageFormatter)replyQueue.Formatter).TargetTypeNames = new string[] { "System.String,mscorlib" };
+
+
+            //Start replyWatcher-Thread
+            AddWatchingThread(replyQueue);
+        }
+
+
+        private void SendRequestQueue(string message, string label)
+        {
+            Message msg = new Message();
+            msg.ResponseQueue = replyQueue;
+            msg.Body = message;
+            msg.Label = label;
+            reuquestQueue.Send(msg);
+            ConsoleWrite(message, label, msg.Id, 0);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        
+        private void ConsoleWrite(string message, string label, string correlationId, int level)
+        {
+            switch (level)
+            {
+                case 0:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(string.Format("[{0}] >> msg-id:{1} ({2}) - {3}",
+                        DateTime.Now.ToString("HH:mm:ss"), correlationId, label, message));
+                    return;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(string.Format("[{0}] << correlation-id:{1} ({2}) - {3}",
+                        DateTime.Now.ToString("HH:mm:ss"), correlationId, label, message));
+                    return;
+            }
+        }
+
+        private MessageQueue SetupChannel(string queueName)
+        {
+            MessageQueue newQueue = null;
+
+            if (!MessageQueue.Exists(queueName))
+                newQueue = MessageQueue.Create(queueName);
+            else
+            {
+                newQueue = new MessageQueue(queueName);
+            }
+            return newQueue;
+        }
+
+        public Thread AddWatchingThread(MessageQueue replyQueue)
+        {
+            Thread Watcher =
+                new Thread(() => AsyncWatchQueue(replyQueue));
+            Watcher.Start();
+            return Watcher;
+        }
+
+        public void AsyncWatchQueue(MessageQueue replyQueue)
+        {
+            Message newMessage = null;
+
+            if (replyQueue == null)
+                return;
+
+            try
+            {
+                if (replyQueue.CanRead)
+                {
+                    while (true)
+                    {
+                        newMessage = replyQueue.Receive();
+
+                        var message = newMessage.Body.ToString();
+                        var label = newMessage.Label;
+                        var correlationId = newMessage.CorrelationId;
+
+                        ConsoleWrite(message, label, correlationId, 1);
+                    }
+                }
+            }
+
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine(string.Format("[{0}] ({1}) {2}", DateTime.Now.ToString("HH:mm:ss"), "Error!",
+                    e.Message));
+            }
+            finally
+
+            {
+                replyQueue.Dispose();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            Console.Write("Payload:");
+            while (true)
+            {
+                d.SendRequestQueue(Console.ReadLine(), "Joe Doe");
+            }
+        }
+    }
+}
---
+++ MSMQReciver/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MSMQReciver
+{
+    class MSMQReciver
+    {
+        private MessageQueue reuquestQueue;
+        private MessageQueue invalidMessageQueue;
+
+        public MSMQReciver()
+        {
+            this.reuquestQueue = SetupChannel(@".\Private$\Request");
+            this.invalidMessageQueue = SetupChannel(@".\Private$\InvalidMessage");
+
+            reuquestQueue.MessageReadPropertyFilter.SetAll();
+            ((XmlMessageFormatter)reuquestQueue.Formatter).TargetTypeNames = new string[] { "System.String,mscorlib" };
+
+            //Start replyWatcher-Thread
+            AddWatchingThread(reuquestQueue);
+        }
+
+        private MessageQueue SetupChannel(string queueName)
+        {
+            MessageQueue newQueue = null;
+
+            if (!MessageQueue.Exists(queueName))
+                newQueue = MessageQueue.Create(queueName);
+            else
+            {
+                newQueue = new MessageQueue(queueName);
+            }
+            return newQueue;
+        }
+
+        public Thread AddWatchingThread(MessageQueue reuquestQueue)
+        {
+            Thread Watcher =
+                new Thread(() => AsyncWatchQueue(reuquestQueue));
+            Watcher.Start();
+            return Watcher;
+        }
+
+        public void AsyncWatchQueue(MessageQueue reuquestQueue)
+        {
+            Message newMessage = null;
+
+            if (reuquestQueue == null)
+                return;
+
+            try
+            {
+                if (reuquestQueue.CanRead)
+                {
+                    while (true)
+                    {
+                        newMessage = reuquestQueue.Receive();
+
+                        ConsoleWrite(newMessage.Body.ToString(), newMessage.Label, newMessage.Id, 0);
+
+                        //main-thread should do this. 
+                        var repsonseMsg = QueueReplier(newMessage);
+
+                        ConsoleWrite(repsonseMsg.Body.ToString(), repsonseMsg.Label, repsonseMsg.Id, 1);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+            }
+
+            catch (
+                ThreadAbortException e
+                )
+            {
+                Console.WriteLine(string.Format("[{0}] ({1}) {2}", DateTime.Now.ToString("HH:mm:ss"), "Error!",
+                    e.Message));
+            }
+            finally
+            {
+                reuquestQueue.Dispose();
+            }
+        }
+
+        private Message QueueReplier(Message message)
+        {
+            MessageQueue replayMessageQueue = message.ResponseQueue;
+
+            //Do logic for student
+
+            Message responseMessage = new Message();
+            responseMessage.Body = randomeChooser();
+            responseMessage.Label = "CPHBusiness";
+            responseMessage.CorrelationId = message.Id;
+
+            replayMessageQueue.Send(responseMessage);
+
+
+            return responseMessage;
+        }
+
+
+        public string randomeChooser()
+        {
+            Random random = new Random();
+
+            if (random.Next(0, 100)%2 == 0)
+                return "Good news!";
+
+            return "Bad news!";
+        }
+
+        private void ConsoleWrite(string message, string label, string id, int level)
+        {
+            switch (level)
+            {
+                case 0:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(string.Format("[{0}] << correlation-id:{1} ({2}) - {3}",
+                        DateTime.Now.ToString("HH:mm:ss"), id, label, message));
+                    return;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(string.Format("[{0}] >> msg-id:{1} ({2}) - {3}",
+                        DateTime.Now.ToString("HH:mm:ss"), id, label, message));
+                    return;
+            }
+    
+        }
+
+        static void Main(string[] args)
+        {
+            MSMQReciver msmq = new MSMQReciver();
+
+            while (true)
+            {
+                Console.WriteLine("Processing other stuff..");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
